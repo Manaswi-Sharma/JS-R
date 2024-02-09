78ed70d770b66d751d36e746ccc9860d58846605
Manaswi-Sharma <101550062+Manaswi-Sharma@users.noreply.github.com>
2024-02-09 19:50:46 +0000

    Variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "manaswi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2
+accountEmail = "ms@ms.com"
+accountPassword = "232323"
+accountCity = "Bengaluru"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
